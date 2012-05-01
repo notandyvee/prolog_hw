8eb04d3d794cf2943bf8f777cef7589407b330a1
Andy Valdez <avaldez@oswego.edu>
2012-05-01 16:55:45 -0400

    added knowledge base on class and subclasses as well as a check to check those

--- hw4.pl
+++ hw4.pl
@@ -1,5 +1,12 @@
+type(string).
+type(int).
+
 instance(_, string).
+instance(_, hashmap).
+instance(_, abstractmap).
 instance(_, int).
+
+subclass(abstractmap, hashmap).
 
 hasMethod(string, length).
 hasMethod(string, println).
@@ -7,5 +14,12 @@ hasMethod(string, charAt).
 hasMethod(string, equals).
 hasMethod(string, indexOf).
 
-check(X, M):= instance(X, C), hasMethod(C, M).
+hasMethod(abstractmap, hashCode).
+hasMethod(abstractmap, toString).
+
+hasMethod(hashmap, size).
+hasMethod(hashmap, isEmpty).
+hasMethod(hashmap, hashCode).
+
+check(X, M):- instance(X, C), hasMethod(C, M); subClass(E, C), hasMethod(E, M).
 
