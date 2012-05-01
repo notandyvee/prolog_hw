f88d25be0561d956b631974d7ae534e36ee6d188
Andy Valdez <avaldez@oswego.edu>
2012-05-01 16:03:00 -0400

    added some rules

--- hw4.pl
+++ hw4.pl
@@ -1,4 +1,11 @@
-instance(string, X).
-instance(int, Y).
+instance(_, string).
+instance(_, int).
 
+hasMethod(string, length).
+hasMethod(string, println).
+hasMethod(string, charAt).
+hasMethod(string, equals).
+hasMethod(string, indexOf).
+
+check(X, M):= instance(X, C), hasMethod(C, M).
 
